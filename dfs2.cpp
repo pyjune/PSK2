6817e694b7058bc00cd98c8f5ae365034eb46848
pyjune <pyjune@naver.com>
2019-08-07 14:46:18 +0900

    Create dfs2.cpp

---
+++ dfs2.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int adj[6][6];
+int visited[6];
+int cnt;
+int minV;
+void dfs(int n, int k);
+void dfs2(int n, int k, int m); // k 목적지, m 노드개수
+void dfs3(int n, int k, int m, int e); // e 지나온 간선 수
+
+int main(void)
+{
+	int V, E;
+	
+	freopen("Text.txt", "r", stdin);
+	scanf("%d %d", &V, &E);
+	minV = E;
+	for (int i = 0; i < E; i++)
+	{
+		int n1, n2;
+		scanf("%d %d", &n1, &n2);
+		adj[n1][n2] = 1;
+		//adj[n2][n1] = 1; // 방향이 없는 경우
+	}
+	//dfs(1, V);
+	//dfs2(1, 4, V);
+	//printf("%d\n", cnt);
+	dfs3(1, 4, V, 0);
+	printf("%d\n", minV);
+}
+
+void dfs3(int n, int k, int m, int e)
+{
+	if (n == k)
+	{
+		if (minV > e)
+			minV = e;
+	}
+	else
+	{
+		visited[n] = 1; // n번 노드에 방문 표시
+		for (int i = 1; i <= m; i++)
+		{
+			if (adj[n][i] != 0 && visited[i] == 0)
+			{
+				dfs3(i, k, m, e + 1);
+			}
+		}
+		visited[n] = 0;
+	}
+}
+void dfs2(int n, int k, int m)
+{
+	if (n == k)
+		cnt++;
+	else
+	{
+		visited[n] = 1; // n번 노드에 방문 표시
+		for (int i = 1; i <= m; i++)
+		{
+			if (adj[n][i] != 0 && visited[i] == 0)
+			{
+				dfs2(i, k, m);
+			}
+		}
+		visited[n] = 0;
+	}
+}
+
+void dfs(int n, int k)
+{
+	visited[n] = 1; // n번 노드에 방문 표시
+	printf("%d ", n);
+	for (int i = 1; i <= k; i++)
+	{
+		if (adj[n][i] != 0 && visited[i] == 0)
+		{
+			dfs(i, k);
+		}
+	}
+}
