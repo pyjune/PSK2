68fa00818e099a54b50a9424645c57a7546e80b6
pyjune <pyjune@naver.com>
2019-08-08 11:31:05 +0900

    Update 미로1.cpp

--- 미로1.cpp
+++ 미로1.cpp
@@ -3,6 +3,7 @@
 int maze[100][100];
 
 int dfs(int r, int c);
+int bfs(int r, int c);
 
 int main(void)
 {
@@ -26,7 +27,8 @@ int main(void)
 				}
 			}
 		}
-		printf("#%d %d\n", tc, dfs(startR, startC));
+		//printf("#%d %d\n", tc, dfs(startR, startC));
+		printf("#%d %d\n", tc, bfs(startR, startC));
 	}
 }
 
@@ -52,4 +54,36 @@ int dfs(int r, int c)
 		}
 		return 0;
 	}
+}
+
+int bfs(int r, int c)
+{
+	int dr[] = { 0, 1, 0, -1 };
+	int dc[] = { 1, 0, -1, 0 };
+	int q[20000];
+	int front = -1;
+	int rear = -1;
+	int visited[100][100] = { 0, };
+	q[++rear] = r;
+	q[++rear] = c;
+	visited[r][c] = 1;
+	while (front != rear)
+	{
+		r = q[++front];
+		c = q[++front];
+		if (maze[r][c] == 3)
+			return 1;
+		for (int i = 0; i < 4; i++)
+		{
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (maze[nr][nc] != 1 && visited[nr][nc] == 0)
+			{
+				q[++rear] = nr;
+				q[++rear] = nc;
+				visited[nr][nc] = visited[r][c] + 1;
+			}
+		}
+	}
+	return 0;
 }
